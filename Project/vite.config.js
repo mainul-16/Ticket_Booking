8c746bf0c2ddbc87300d51a22f4fb9dac4cca28c
mainul-16 <mainulislam160105@gmail.com>
2025-07-13 19:38:02 +0530

    fix

--- Project/vite.config.js
+++ Project/vite.config.js
@@ -7,5 +7,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/Project",
 })
